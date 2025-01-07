20e610694daf3c81e36babce087df7d55facf177
ansu555 <anik200365@gmail.com>
2025-01-07 10:30:58 +0530

    datatypes

---
+++ Practice/Js/dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) // we are using node.js noT browser
+
+let name = "anik"
+let age = 20 // number 2 to poweer 53 max limit 
+let isLoggedIn = false
+
+
+// if my number is too big then use bigint
+//string =>""
+//boolean => true/false
+//null => stand alone value
+//undefined => now value doesn't assigned
+//symbol => for unique value
+//object => collection of value
+
+console.log(typeof undefined ); //output undefined
+console.log(typeof null); // output object
+
