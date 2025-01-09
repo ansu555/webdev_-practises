099d826c6f4536dafb32c06138bbe12727ee8acb
ansu555 <anik200365@gmail.com>
2025-01-09 12:10:29 +0530

    Add initial implementation of number and math operations examples

---
+++ Practice/Js/06_nums_and_maths.js
@@ -0,0 +1,18 @@
+const score =400
+console.log(score);
+
+const balance = new Number(100) 
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.84566
+
+console.log(otherNumber.toPrecision(3)); // => output 124
+
+const hundreds = 1000000
+
+console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++++++++++++++++++++ maths ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
