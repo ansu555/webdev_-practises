1d6fd88d99316e25ae77254b445f58bf27771732
ansu555 <anik200365@gmail.com>
2025-01-11 11:08:45 +0530

    Fix string slicing example to demonstrate behavior with negative indices

--- Practice/Js/test.js
+++ Practice/Js/test.js
@@ -1,7 +1,6 @@
-let a = "1"
-let b = 2
-let c = b+a
+const gamename = new String('anik2003-valo-chele');
 
-console.log(typeof c);
-console.log(c);
 
+const anotherString = gamename.slice(-19, 4); 
+// Extracts a portion of the string. Negative values count from the end of the string.
+console.log(anotherString); // Logs an empty string because the start index is greater than the end index.
