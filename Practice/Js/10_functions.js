ff9e05d8704c2e758697d0bb271e3d36fa66e557
ansu555 <anik200365@gmail.com>
2025-01-14 11:15:47 +0530

    Add functions for adding numbers and logging user messages

---
+++ Practice/Js/10_functions.js
@@ -0,0 +1,18 @@
+// function addTwoNumbers(number1,number2)
+// {
+//     console.log(number1+number2);
+// }
+function addTwoNumbers(number1,number2)
+{
+    return number1+number2
+}
+const result=addTwoNumbers(3,5);
+console.log(result);
+
+
+function loginUserMessage(username)
+{
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage("anik"));
