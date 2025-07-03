b97b5db03f714d3f944f6b03d0aefe4f3333e5f1
ansu555 <anik200365@gmail.com>
2025-07-03 14:58:12 +0530

    Refactor date handling examples; replace string manipulations with date methods and improve output formatting

--- Practice/Js/test.js
+++ Practice/Js/test.js
@@ -1,17 +1,18 @@
-const temp = "Anik valo chele";
+let myDate = new Date(); // Creating a new date object with the current date and time
+console.log(typeof myDate); // Type of `myDate` is "string" since Date
 
-const url = "https://myPortfolio/Anik%20Das";
+console.log(myDate.toString()); // Outputs the current timestamp in milliseconds since January 1, 1970
+console.log(myDate.toDateString()); // Outputs the date in a human-readable format (e.g., "Mon Jan 09 2025")
+console.log(myDate.toTimeString()); // Outputs the time in a human-readable format (e.g., "00:00:00 GMT+0000 (Coordinated Universal Time)")
+console.log(myDate.toLocaleString()); // Outputs the date and time in the local timezone format
 
-console.log(temp.replace('valo','baje'));
+let myCreation= new Date("2025-07-3")
+console.log(myCreation);
+console.log(myCreation.toDateString()); // Outputs the date in a human-readable format (e.g., "Mon Jan 09 2025")
 
-console.log(temp.split(' '));
+let myTimeStamp = Date.now() // Gets the current timestamp (milliseconds since January 1, 1970
 
-console.log(temp.split(' ')[0]);
-
-console.log(temp.toUpperCase());
-console.log(temp.charAt(3)); // Finds the character at index 3 (0-based indexing).
-console.log(temp.indexOf('k')); // Finds the position of the character 'k'. 
-
-console.log(url.includes('rog'));
+console.log(myTimeStamp); // Outputs the current timestamp in milliseconds (e.g., 1683448768474)
 
 
+console.log(myTimeStamp.getTime()); // Returns the timestamp of `myCreation` in milliseconds since
